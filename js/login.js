ed3cc936f451efcbba69ffd582a0048ca7ca65ae
hhyukk <hhyukk1273@gmail.com>
2023-07-12 22:16:04 +0900

    Add Login Function
    로그인 기능 추가

---
+++ js/login.js
@@ -0,0 +1,42 @@
+const loginForm = document.querySelector("#login-form");
+const loginFormSignUpButton = document.querySelector(
+  "#login-form input:nth-child(4)"
+);
+const loginFormLogInButton = document.querySelector(
+  "#login-form input:nth-child(3)"
+);
+const loginFormId = document.querySelector("#login-form input:first-child");
+const loginFormPassword = document.querySelector(
+  "#login-form input:nth-child(2)"
+);
+
+function checkUserInformation(event) {
+  if (localStorage.getItem(loginFormId.value) == null) {
+    if (loginFormId.value === "") {
+      alert("아이디를 확인해주세요");
+    } else if (loginFormPassword.value === "") {
+      alert("비밀번호를 확인해주세요.");
+    } else {
+      localStorage.setItem(loginFormId.value, loginFormPassword.value);
+      alert("회원가입이 완료되었습니다.");
+    }
+  } else {
+    alert("이미 있는 아이디입니다.");
+  }
+}
+
+loginFormSignUpButton.addEventListener("click", checkUserInformation);
+
+function logIn(event) {
+  if (localStorage.getItem(loginFormId.value) === null) {
+    alert("아이디가 틀렸습니다.");
+  } else if (
+    localStorage.getItem(loginFormId.value) !== loginFormPassword.value
+  ) {
+    alert("아이디 혹은 비밀번호가 다릅니다.");
+  } else {
+    loginForm.action = "friends.html";
+    loginFormLogInButton.addEventListener("submit");
+  }
+}
+loginFormLogInButton.addEventListener("click", logIn);
